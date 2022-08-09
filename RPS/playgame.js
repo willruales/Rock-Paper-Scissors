49557ad0fc0767d0eac21e7d13a5ddb98d294b5d
William Ruales <ruales@hotmail.co.uk>
2022-08-09 10:25:32 -0500

    functions solved through variables

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -4,28 +4,25 @@ console.log('hello helpp');
 const game = [ 'rock', 'paper', 'scissors']; 
 
 
+function playgame( computer, user) {
+  if (computer == 'rock' && user == 'paper'){console.log("rock beats paper");}
+else if (computer == 'paper' && user == 'paper'){console.log("paper beats rock");}
+else {console.log('WORKS!');}}
 
-function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
+playgame()
 
-    if  (choice == ("1")) {console.log('rock');}
-    else if (choice == "2") {console.log('paper');}
-    else {console.log('scissor');}}
-    
-getcomputerchoice();
+const computerchoice = function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
 
-function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
+    if  (choice == "1") return 'rock';
+    else if (choice == "2") return 'paper';
+    else return 'scissor';}
+
+let computer = computerchoice(); 
+
+console.log(computer)
+
+const userchoice = function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
 return userinput}
 
-console.log(getuserchoice());
-
-/*function playgame( getcomputerchoice, getuserchoice) {
-  if (getcomputerchoice == 'rock' && getuserchoice == 'paper'){console.log("rock beats paper");}
-else if (getcomputerchoice == 'paper' && getuserchoice == 'paper'){console.log("paper beats rock");}
-else {console.log('WORKS!');} */
-
-function playgame(getcomputerchoice, getuserchoice) {
-    let outcome = getcomputerchoice() + getuserchoice()
-    return outcome};
-
-console.log(playgame());
-qqqqqq
+let user = userchoice()
+console.log(user);

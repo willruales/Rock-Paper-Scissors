dc842f6ad59107ff161653b7915ea35c5365f2bd
William Ruales <ruales@hotmail.co.uk>
2022-08-10 07:42:53 -0500

    play function fix

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,28 +1,26 @@
 console.log('hello helpp');
 
 
-const game = [ 'rock', 'paper', 'scissors']; 
+const userchoice = function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
+return userinput}
 
 
-function playgame( computer, user) {
-  if (computer == 'rock' && user == 'paper'){console.log("rock beats paper");}
-else if (computer == 'paper' && user == 'paper'){console.log("paper beats rock");}
-else {console.log('WORKS!');}}
 
-playgame()
+
+const play =function playgame(computer,user){  //learn syntax for functions and variables//
+  user = userchoice()
+  if (user === 'rock') return 'win'
+  if (user === 'paper' && computer === computer) return 'win1'
+  else return 'lose'
+
+}
+let p = play()
+console.log(p)
 
 const computerchoice = function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
 
     if  (choice == "1") return 'rock';
     else if (choice == "2") return 'paper';
-    else return 'scissor';}
+    else return 'scissors';}
 
-let computer = computerchoice(); 
-
-console.log(computer)
-
-const userchoice = function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
-return userinput}
-
-let user = userchoice()
-console.log(user);
+let computer= computerchoice()

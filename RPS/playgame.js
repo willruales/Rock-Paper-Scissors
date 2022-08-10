b67d9dd26fc9fc575db34aff5d408836abd21bd3
William Ruales <ruales@hotmail.co.uk>
2022-08-10 09:12:02 -0500

    play function improve

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,26 +1,28 @@
 console.log('hello helpp');
 
+let computer= getcomputerchoice()
+let user = getuserchoice()
 
-const userchoice = function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
+playgame(user, computer)
+
+function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
 return userinput}
 
+function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
+
+  if  (choice == "1") return 'rock';
+  else if (choice == "2") return 'paper';
+  else return 'scissors';}
 
 
+function playgame(user,computer){  //learn syntax for functions and variables//
+  
 
-const play =function playgame(computer,user){  //learn syntax for functions and variables//
-  user = userchoice()
-  if (user === 'rock') return 'win'
-  if (user === 'paper' && computer === computer) return 'win1'
+  if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') alert ('win')
+  if (user === 'rock' && computer === 'paper') alert ('loser');
+  if (user === 'paper' && computer === 'scissors')alert ('loser'); 
+  if  (user === 'scissors' && computer === 'rock')alert ('loser'); 
+  
   else return 'lose'
 
 }
-let p = play()
-console.log(p)
-
-const computerchoice = function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
-
-    if  (choice == "1") return 'rock';
-    else if (choice == "2") return 'paper';
-    else return 'scissors';}
-
-let computer= computerchoice()

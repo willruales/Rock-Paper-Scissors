b218bf35b7d2f5e92a4a4f7df4b757b2be405b01
William Ruales <ruales@hotmail.co.uk>
2022-08-14 08:25:57 -0500

    lets see

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,28 +1,41 @@
 console.log('hello helpp');
 
-let computer= getcomputerchoice()
+
+
 let user = getuserchoice()
-
-playgame(user, computer)
-
-function getuserchoice (){let userinput = window.prompt("rock,paper,scissors");
+function getuserchoice(){let userinput = window.prompt("rock,paper,scissors");
 return userinput}
 
-function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
+
+function playgame(){      
+let playr=playround()
+
+
+
+
+function getcomputerchoice(min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
 
   if  (choice == "1") return 'rock';
   else if (choice == "2") return 'paper';
   else return 'scissors';}
 
 
-function playgame(user,computer){  //learn syntax for functions and variables//
-  
 
-  if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') alert ('win')
-  if (user === 'rock' && computer === 'paper') alert ('loser');
-  if (user === 'paper' && computer === 'scissors')alert ('loser'); 
-  if  (user === 'scissors' && computer === 'rock')alert ('loser'); 
-  
-  else return 'lose'
+
+function playround(user,computer){ 
+  let computer= getcomputerchoice()
+    let result = ''
+    if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') {return result +1}
+    else if (user === 'rock' && computer === 'paper' ||user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { return result +2;}
+  else if (user === computer){ return result +3 }
+    
+    else return alert ('please try again!');
+    }
+
+return console.log(playr)
 
 }
+
+//console.log(playround(user,computer))
+
+playgame()

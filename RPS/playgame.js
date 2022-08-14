5f8ac45caf6f1e4c0e2c65fe1f9db0abb1d22b09
William Ruales <ruales@hotmail.co.uk>
2022-08-14 16:36:24 +0100

    when to use let

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -3,10 +3,10 @@ console.log('hello helpp');
 let computer= getcomputerchoice
 
 
-const user1 = function getuserchoice (){  let userinput = window.prompt("rock,paper,scissors");
+function getuserchoice (){  let userinput = window.prompt("rock,paper,scissors");
 return userinput}
-let user = user1()
-console.log(user)
+
+
 
 //computer choice
 
@@ -20,13 +20,14 @@ function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3)
 
 // playround 
 
-function playgame(){      
-let playr=playround()
+
+function playgame(){
 
 
-
-function playround(user,computer){ 
-    let result = ''
+const playr = function playround(user,computer){ 
+    let result = 0
+     user = getuserchoice()
+    computer = getcomputerchoice()
     if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') {return result +2}
     else if (user === 'rock' && computer === 'paper' ||user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { return result +5;}
   else if (user === computer){ return result +1 }
@@ -34,10 +35,8 @@ function playround(user,computer){
     else return alert ('please try again!');
     }
 
-return console.log(playr)
+let play = playr()
 
-}
 
-//console.log(playround(user,computer))
-
+console.log(play)}
 playgame()

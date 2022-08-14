f16a3fb95f65c35e917e250227126f7f3e47fca2
William Ruales <ruales@hotmail.co.uk>
2022-08-14 17:24:10 +0100

    added no var

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,6 +1,5 @@
 console.log('hello helpp');
 
-let computer= getcomputerchoice
 
 
 function getuserchoice (){  let userinput = window.prompt("rock,paper,scissors");
@@ -20,17 +19,17 @@ function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3)
 
 // playround 
 
-
+ 
 function playgame(){
 
 
 const playr = function playround(user,computer){ 
     let result = 0
-     user = getuserchoice()
+    user = getuserchoice()
     computer = getcomputerchoice()
     if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') {return result +2}
     else if (user === 'rock' && computer === 'paper' ||user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { return result +5;}
-  else if (user === computer){ return result +1 }
+  else if (user === computer){ return result +66}
     
     else return alert ('please try again!');
     }

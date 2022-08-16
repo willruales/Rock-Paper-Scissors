865b7effafa816c5c52f782d6cf360f90c3860a0
William Ruales <ruales@hotmail.co.uk>
2022-08-16 16:49:45 +0100

    names shortened

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,28 +1,32 @@
 console.log('hello helpp');
-//computer choice
+
 
 function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
 
-  if  (choice == "1") return 'rock';
-  else if (choice == "2") return 'paper';
-  else return 'scissors';}
+  if  (choice == "1") return 'r';
+  else if (choice == "2") return 'p';
+  else return 's';}
 
 // playround
  
 function playgame(){
+  
   function getuserchoice (){  let userinput = window.prompt("rock,paper,scissors");
   return userinput}
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 2; i++) {
   getuserchoice();
+
   }
 
 const playr = function playround(user,computer){ 
-    let result = 0
+    let result = 0;
+
     user = getuserchoice()
+
     computer = getcomputerchoice()
-    if (user === 'paper' && computer === 'rock' || user ==='rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') {return result +2}
-    else if (user === 'rock' && computer === 'paper' ||user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { return result +5;}
-  else if (user === computer){ return result +66}
+    if (user === 'p' && computer === 'r' || user ==='r' && computer === 's' || user === 's' && computer === 'p') {return result =+1}
+    else if (user === 'r' && computer === 'p' ||user === 'p' && computer === 's' || user === 's' && computer === 'rock') { return result =-1;}
+  else if (user === computer){ return result =+0}
     
     else return alert ('please try again!');
     }

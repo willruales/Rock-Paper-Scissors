16f3ef962269364f77dfc45c6b358c14468c86b4
William Ruales <ruales@hotmail.co.uk>
2022-09-22 14:12:42 +0100

    succesful array add

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,6 +1,6 @@
 console.log('hello helpp');
 //computer choice
-let result = '';
+let result = []
 function getcomputerchoice(min, max) {
   let choice = Math.floor((Math.random)() * 3) + 1;
 
@@ -22,9 +22,9 @@ function playgame() {
     //let result = '';
     user = getuserchoice();
     computer = getcomputerchoice()
-    if (user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') { result = +1 }
-    else if (user === 'rock' && computer === 'paper' || user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { result = -1; }
-    else if (user === computer) { return result = +0 }
+    if (user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') { result.push(1) }
+    else if (user === 'rock' && computer === 'paper' || user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { result.push(-1) }
+    else if (user === computer) { result.push(0) }
 
     else return alert('please try again!');//make return
     return result;
@@ -32,19 +32,20 @@ function playgame() {
 
   let play = playr()
 
-  console.log(result);
+
   return play;
 
 }
 
 
 let score = function getScore() {
+  let sum = 0;
   playgame()
-  for (i = 0; i < 2; i++) {
+  for (i = 0; i < 4; i++) {
     playgame()
-
+    sum += result[i];
   }
-
+  console.log(sum);
 }
 score()
 

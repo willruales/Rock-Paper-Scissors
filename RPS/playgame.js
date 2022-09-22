b82bf112b6f8c65ffc65dcd6093bbc8a8d778180
William Ruales <ruales@hotmail.co.uk>
2022-09-22 17:07:42 +0100

    added new array

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,38 +1,48 @@
 console.log('hello helpp');
+//computer choice
+let result = []
+function getcomputerchoice(min, max) {
+  let choice = Math.floor((Math.random)() * 3) + 1;
+
+  if (choice == "1") return 'rock';
+  else if (choice == "2") return 'paper';
+  else return 'scissors';
+}
 
 
-function getcomputerchoice (min,max) {let choice = Math.floor((Math.random)()*3) + 1; 
+function playgame() {
 
-  if  (choice == "1") return 'r';
-  else if (choice == "2") return 'p';
-  else return 's';}
-
-// playround
- 
-function playgame(){
-  
-  function getuserchoice (){  let userinput = window.prompt("rock,paper,scissors");
-  return userinput}
-  for (let i = 0; i < 2; i++) {
-  getuserchoice();
-
+  function getuserchoice() {
+    let userinput = window.prompt("rock,paper,scissors");
+    return userinput
   }
 
-const playr = function playround(user,computer){ 
-    let result = 0;
-
-    user = getuserchoice()
-
+  const playr = function playround(user, computer) {
+    //let result = '';
+    user = getuserchoice();
     computer = getcomputerchoice()
-    if (user === 'p' && computer === 'r' || user ==='r' && computer === 's' || user === 's' && computer === 'p') {return result =+1}
-    else if (user === 'r' && computer === 'p' ||user === 'p' && computer === 's' || user === 's' && computer === 'rock') { return result =-1;}
-  else if (user === computer){ return result =+0}
-    
-    else return alert ('please try again!');
-    }
+    if (user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') { result.push(1) }
+    else if (user === 'rock' && computer === 'paper' || user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { result.push(-1) }
+    else if (user === computer) { result.push(0) }
 
-let play = playr()
+    else return alert('please try again!');//make return
+    return result;
+  }
 
+  let play = playr()
 
-console.log(play)}
-playgame()
+  return play;
+
+}
+
+let score = function getScore() {
+  let sum = 0;
+  playgame()
+  for (i = 0; i < 4; i++) {
+    playgame()
+    sum += result[i];
+  }
+  console.log(sum);
+}
+score()
+

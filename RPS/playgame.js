5c948a69678f454086969578ceaf3aaa6d42bfc4
William Ruales <ruales@hotmail.co.uk>
2022-12-13 18:57:41 +0000

    best version for ui

--- RPS/playgame.js
+++ RPS/playgame.js
@@ -1,5 +1,15 @@
-let result = []
 let buttonArray = document.querySelectorAll('Button');
+
+
+let arr = []
+
+function play(e) {
+  const userChoice = e.target.id;
+  const computerChoice = getcomputerchoice();
+  const winner = getWinner(userChoice, computerChoice);
+  console.log(userChoice, computerChoice, winner)
+
+}
 
 function getcomputerchoice(min, max) {
   let choice = Math.floor((Math.random)() * 3) + 1;
@@ -9,37 +19,15 @@ function getcomputerchoice(min, max) {
   else return 'scissors';
 }
 
-function getuserchoice(e) {
-  console.log(e.target.id);
-}
+function getWinner(user, computer) {
 
-
-
-
-const playround = function playround(e, user, computer) {
-  user = e.target.id;
-  computer = getcomputerchoice()
-  if (user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') { result.push(1) }
-  else if (user === 'rock' && computer === 'paper' || user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { result.push(-1) }
-  else if (user === computer) { result.push(0) }
+  if (user === 'paper' && computer === 'rock' || user === 'rock' && computer === 'scissors' || user === 'scissors' && computer === 'paper') { return 'win' }
+  else if (user === 'rock' && computer === 'paper' || user === 'paper' && computer === 'scissors' || user === 'scissors' && computer === 'rock') { return 'lose' }
+  else if (user === computer) { return 'draw' }
 
   else return alert('please try again!');//make return
-  return result;
-  //console.log(result)
+
 }
 
 
-
-
-let score = function getScore() {
-  let sum = 1;
-  buttonArray.forEach((buttonArray) => { buttonArray.addEventListener('click', playround) })
-  for (i = 0; i < 4; i++) {
-
-    sum += result[i];
-  }
-  console.log(sum);
-  //console.log(result)
-}
-score()
-
+buttonArray.forEach((buttonArray) => { buttonArray.addEventListener('click', play) })
